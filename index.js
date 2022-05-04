148096fb433af02a9848d80c8ef41d712d0a98b5
Douglas Pith <pithdouglas@gmail.com>
2022-05-04 15:43:42 -0700

    added input and output forms

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
-
+const multer = require("multer");
+const upload = multer();
 
 // Add database package and connection string (can remove ssl)
 const { Pool } = require('pg');
@@ -42,3 +43,62 @@ app.get("/", (req, res) => {
         });
     });
 });
+
+
+// Display and process the input form
+app.get("/input", (req, res) => {
+    res.render("input");
+ });
+ 
+ app.post("/input",  upload.single('filename'), (req, res) => {
+     if(!req.file || Object.keys(req.file).length === 0) {
+         message = "Error: Import file not uploaded";
+         return res.send(message);
+     };
+     //Read file line by line, inserting records
+     const buffer = req.file.buffer; 
+     const lines = buffer.toString().split(/\r?\n/);
+ 
+     lines.forEach(line => {
+          //console.log(line);
+          product = line.split(",");
+          //console.log(product);
+          const sql = "INSERT INTO PRODUCT(prod_id, prod_name, prod_desc, prod_price) VALUES ($1, $2, $3, $4)";
+          pool.query(sql, product, (err, result) => {
+              if (err) {
+                  console.log(`Insert Error.  Error message: ${err.message}`);
+              } else {
+                  console.log(`Inserted successfully`);
+              }
+         });
+     });
+     message = `Processing Complete - Processed ${lines.length} records`;
+     res.send(message);
+ });
+
+// Display and process the output form
+app.get("/output", (req, res) => {
+    var message = "";
+    res.render("output",{ message: message });
+});
+
+
+app.post("/output", (req, res) => {
+    const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
+    pool.query(sql, [], (err, result) => {
+        var message = "";
+        if(err) {
+            message = `Error - ${err.message}`;
+            res.render("output", { message: message })
+        } else {
+            var output = "";
+            result.rows.forEach(product => {
+                output += `${product.prod_id},${product.prod_name},${product.prod_desc},${product.prod_price}\r\n`;
+            });
+            res.header("Content-Type", "text/csv");
+            res.attachment("export.csv");
+            return res.send(output);
+        };
+    });
+});
+
